3c950308a6d0769ff844853d0536685ea0919ab2
unknown <a.petkovpetkov@gmail.com>
2021-10-15 10:02:35 +0300

    Adding Build in function first part

--- Training/training.sql
+++ Training/training.sql
@@ -34,3 +34,41 @@ ELSE middle_name
 END)
  ,' ', last_name) AS `full_name`, job_title
 FROM employees;
+
+# Build In Function #
+
+SELECT first_name, last_name FROM employees
+WHERE first_name LIKE 'sa%'
+ORDER BY employee_id;
+
+SELECT first_name, last_name FROM employees
+WHERE last_name LIKE '%ei%'
+ORDER BY employee_id;
+
+SELECT first_name FROM employees
+WHERE department_id IN (3, 10)
+AND YEAR (hire_date) BETWEEN 1995 AND 2005
+ORDER BY employee_id;
+
+SELECT first_name, last_name FROM employees
+WHERE job_title NOT LIKE '%engineer%'
+ORDER BY employee_id;
+
+SELECT name FROM towns
+WHERE length(name) BETWEEN 5 AND 6
+ORDER BY name;
+
+SELECT town_id, name FROM towns
+WHERE name LIKE 'M%'
+OR name LIKE 'K%'
+OR name LIKE 'B%'
+OR name LIKE 'E%'
+ORDER BY name;
+
+SELECT town_id, name FROM towns
+WHERE NOT (
+`name` LIKE('R%') OR
+`name` LIKE('B%') OR
+`name` LIKE('D%')
+)
+ORDER BY `name`;
